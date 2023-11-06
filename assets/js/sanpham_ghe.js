5f99d1d6df381727b805bf054b3c9d7a09bbe790
Bùi Quang Phú <101036324+PhuBuii@users.noreply.github.com>
2023-11-06 13:48:23 +0700

    update landingpage, share container, sanpham_ghe & modified ghe_detail

--- assets/js/sanpham_ghe.js
+++ assets/js/sanpham_ghe.js
@@ -1,3 +1,20 @@
+// Define the number of cards to show initially and incrementally
+let cardsToShow = 12;
+const cardContainer = document.querySelector(".product-cards");
+const loadMoreButton = document.getElementById("load-more-button");
+const productCards = Array.from(document.querySelectorAll(".product-card"));
+// Function to show the next batch of product cards
+function showMoreCards() {
+  for (let i = 0; i < cardsToShow; i++) {
+    if (productCards.length > 0) {
+      productCards.shift().style.display = "block";
+    } else {
+      loadMoreButton.style.display = "none";
+      break;
+    }
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   const filterButtons = document.querySelectorAll(".filter-bar button");
   const productCards = document.querySelectorAll(".product-card");
@@ -20,13 +37,27 @@ document.addEventListener("DOMContentLoaded", function () {
           card.style.display = "block";
           card.classList.add("show");
         });
+        // Initially, hide all product cards and show the first batch
+        productCards.forEach((card, index) => {
+          if (index >= cardsToShow) {
+            card.style.display = "none";
+          }
+        });
+
+        // Add a click event listener to the "Load More" button
+        loadMoreButton.addEventListener("click", showMoreCards);
+
+        // Show the initial batch of product cards
+        showMoreCards();
         noResultsMessage.style.display = "none";
+        loadMoreButton.style.display = "block";
       } else {
         const filteredCards = document.querySelectorAll(
           `[data-category="${filterValue}"]`
         );
         if (filteredCards.length === 0) {
           noResultsMessage.style.display = "block";
+          loadMoreButton.style.display = "none";
           filterName.textContent = button.textContent;
         } else {
           filteredCards.forEach((card) => {
@@ -34,6 +65,7 @@ document.addEventListener("DOMContentLoaded", function () {
             card.classList.add("show");
           });
           noResultsMessage.style.display = "none";
+          loadMoreButton.style.display = "none";
         }
       }
     });
@@ -46,3 +78,15 @@ $(document).ready(function () {
     $(this).addClass("active"); // Thêm lớp "active" cho nút được chọn
   });
 });
+// Initially, hide all product cards and show the first batch
+productCards.forEach((card, index) => {
+  if (index >= cardsToShow) {
+    card.style.display = "none";
+  }
+});
+
+// Add a click event listener to the "Load More" button
+loadMoreButton.addEventListener("click", showMoreCards);
+
+// Show the initial batch of product cards
+showMoreCards();
