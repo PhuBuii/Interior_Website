cf583080206b7384dd65ab8364ce6dd54a517c53
Bùi Quang Phú <101036324+PhuBuii@users.noreply.github.com>
2023-11-21 09:09:01 +0700

    merge code

--- assets/js/landingpage.js
+++ assets/js/landingpage.js
@@ -90,3 +90,21 @@ for (const mconcept_show of mconcept_shows) {
 }
 modal.addEventListener("click", closePopup);
 modal_container.addEventListener("click", closePopup);
+
+// Khởi tạo Swiper với hiệu ứng parallax
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
