7dba44ca5c7e1782fdb780521bd532303d0264e5
Bùi Quang Phú <101036324+PhuBuii@users.noreply.github.com>
2023-11-22 15:43:40 +0700

    add loading & connect

--- assets/js/sanpham_detail.js
+++ assets/js/sanpham_detail.js
@@ -27,3 +27,42 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+function selectColor(colorId) {
+  var colors = document.getElementsByClassName("color");
+
+  // Xóa lớp "selected" khỏi tất cả các màu
+  for (var i = 0; i < colors.length; i++) {
+    colors[i].classList.remove("selected");
+  }
+
+  // Thêm lớp "selected" vào màu được chọn
+  var selectedColor = document.getElementById(colorId);
+  selectedColor.classList.add("selected");
+}
+document.addEventListener("DOMContentLoaded", function () {
+  // Chọn tùy chọn đầu tiên khi trang web được tải
+  var defaultOption = document.querySelector(".filter p:first-child");
+  defaultOption.click();
+});
+function showContent(contentId) {
+  // Ẩn tất cả các nội dung và loại bỏ lớp "selected" từ tất cả các filter-item
+  var filterContents = document.getElementsByClassName("filter-content");
+  var filterItems = document.getElementsByClassName("filter-item");
+
+  for (var i = 0; i < filterContents.length; i++) {
+    filterContents[i].style.display = "none";
+    filterItems[i].classList.remove("selected");
+  }
+
+  // Hiển thị nội dung tương ứng với mục được chọn và thêm lớp "selected" vào filter-item
+  var selectedContent = document.getElementById(contentId);
+  selectedContent.style.display = "block";
+
+  // Tìm filter-item tương ứng và thêm lớp "selected"
+  for (var i = 0; i < filterItems.length; i++) {
+    if (filterItems[i].getAttribute("onclick").includes(contentId)) {
+      filterItems[i].classList.add("selected");
+      break; // Dừng vòng lặp sau khi thêm lớp "selected" vào filter-item
+    }
+  }
+}
