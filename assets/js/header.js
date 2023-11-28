e5b643d89132aa387a8a6903cbd2d5850c259c2d
Bùi Quang Phú <101036324+PhuBuii@users.noreply.github.com>
2023-11-28 12:26:03 +0700

    fix login & create logout

--- assets/js/header.js
+++ assets/js/header.js
@@ -23,7 +23,11 @@ function signin() {
   // Nếu đúng, thực hiện các thay đổi sau
   if (passwordInput == "123") {
     // Thay đổi nội dung và hiển thị tên người dùng
-    usernameShow.innerText = usernameInput;
+    if (usernameInput.length > 7) {
+      usernameShow.innerText = usernameInput.slice(0, 6) + "...";
+    } else {
+      usernameShow.innerText = usernameInput;
+    }
 
     // Ẩn icon user
     userIcon.style.display = "none";
@@ -38,10 +42,15 @@ function signin() {
       "Đăng nhập không thành công. Vui lòng kiểm tra lại tên người dùng và mật khẩu."
     );
   }
+  document.getElementById("username").value = "";
+  document.getElementById("password").value = "";
 }
 
 function signup() {
   alert("Chúc mừng bạn đã đăng ký thành công");
+}
+function sendEmail() {
+  alert("Chúng tôi đã gửi mã đổi password đến địa chỉ mail của bạn!");
 }
 
 function closeLoginPopUp(event) {
@@ -69,5 +78,29 @@ function addItem(event) {
   alert("Bạn đã nhấn nút");
   if (event) {
     event.preventDefault();
+  }
+}
+function showUserPopup() {
+  var user_popup = document.getElementById("userPopup");
+  user_popup.style.display = "block";
+}
+function logout() {
+  var usernameShow = document.getElementById("username_show");
+  var userIcon = document.querySelector("#user i");
+  var userPopup = document.getElementById("userPopup");
+  // Ẩn icon user
+  userIcon.style.display = "contents";
+  usernameShow.style.display = "none";
+  userPopup.style.display = "none";
+}
+function closeUserPopUp(event) {
+  var userContainer = document.getElementById("user-popup-container");
+  var userPopup = document.getElementById("userPopup");
+  if (
+    !userContainer.contains(event.target) &&
+    !event.target.classList.contains("ghost")
+  ) {
+    // Đóng popup
+    userPopup.style.display = "none";
   }
 }
